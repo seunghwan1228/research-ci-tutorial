37fb0d7ca0f4d0d164af2149b6ee46e64a2c5460
seunghwan <seunghwan1228@gmail.com>
2021-06-12 16:25:40 +0900

    black

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def    helloworld(a):
-    print(f"Hellow world! {a}!")# asdfsadfsacsdacsa
+def helloworld(a):
+    print(f"Hellow world! {a}!")  # asdfsadfsacsdacsa
+
 
 if __name__ == "__main__":
     helloworld("Han")
