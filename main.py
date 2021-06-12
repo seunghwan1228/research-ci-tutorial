e5b14ca9ec41b5e406028c8b3ed6bf33d6b1e431
seunghwan <seunghwan1228@gmail.com>
2021-06-12 16:58:55 +0900

    refactor

--- main.py
+++ main.py
@@ -2,9 +2,6 @@ def helloworld(a):
     msg = f"Hellow world! {a}!"
     return msg
 
-def helloworld(a):
-    msg = f"Hellow world! {a}!"
-    return msg
 
 if __name__ == "__main__":
     helloworld("Han")
